daa7dd05e1d1dbf58842d4c1b74f2804aa3b0c22
hxf <hxf20001121@gmail.com>
2025-08-17 14:51:29 +0800

    main

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import "dotenv/config"
 import express from 'express';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 let tea = []
@@ -49,6 +50,7 @@ app.get("/tea/:id", (req, res) => {
   res.status(200).send(Tea)
 })
 app.listen(port, () => {
+  console.log(port)
   console.log("This is  work")
 })
 
